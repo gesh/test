06c7bc97b4bba667452e7429e5b2cc3246444971
Georgis Andonis <georgis.andonis@gmail.com>
2023-06-15 17:25:06 +0300

    test

---
+++ package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "SessionStack",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "SessionStack",
+            targets: ["SessionStack"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .binaryTarget(
+            name: "SessionStack",
+            url: "https://storage.googleapis.com/ios-swift-sdk/SessionStack.zip",
+            checksum: "5f475747e40acf0b3fae82dd9e07c900429f3ac3c3a14f7e45a044f53d432a6f"
+        ),
+    ]
+)

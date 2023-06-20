e0b075ad8f2395d99e9858a1ea09ab9ae2f296ec
Georgis Andonis <georgis.andonis@gmail.com>
2023-06-20 16:55:08 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SessionStack",
-            url: "https://storage.googleapis.com/ios-swift-sdk/SessionStack.zip",
-            checksum: "cdf319e90acd5ff04d1eb07c53c3ec29e32033454c2a6778d1d8df25b993778b"
+            url: "https://storage.googleapis.com/ios-swift-sdk/SessionStack-1.0.0.zip",
+            checksum: "52ad7ab211d032816a93f24ca7d266ad667c038abd41e29fe37e7f5659236431"
         ),
     ]
 )
